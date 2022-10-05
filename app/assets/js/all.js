c9889741a6e0b51cf08e55a3fa85ece0ccfdde9a
lily <a0976198586@gmail.com>
2022-10-05 12:24:35 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -7,6 +7,8 @@ const list = document.querySelector('.list');
 // 預設為第一個 tab (分頁標籤)為全部
 let toggleTab = 'all';
 
+
+
 // 新增
 const btn = document.querySelector('.btn_add');
 const input = document.querySelector('#input');
@@ -25,3 +27,34 @@ btn.addEventListener('click', function(e){
   // renderData();
 });
 
+const cardList = document.querySelector('.card_list');
+
+// 根據當前 tab 渲染畫面  初始值
+function renderData() {
+//如果沒有 data 默認把整個區塊隱藏
+  if(!data.length){
+    cardList.style.display = 'none';
+    return;
+  }
+  //如果是顯示的
+  cardList.style.display= 'block';
+  let str = '';
+  let count = 0; //初始值
+  data.forEach(function(item, index){
+    if(!item.checked){
+      // 計算待完成項目有幾個
+      count += 1;
+      if(toggleTab == 'all' || toggleTab == 'work'){
+        str += `
+        <li>
+          <label for="" class="checkbox" for=''>
+            <input type="checkbox" data-num="${index}">
+            <span>${item.content}</span>
+          </label>
+          <a href="#" class="delete" data-num="${index}"></a>
+      </li>
+        `;
+      }
+    }
+  });
+}
