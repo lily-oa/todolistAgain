da16333d8bfe8f74b0cf9f89ff04d24e484d3d7a
lily <a0976198586@gmail.com>
2022-10-05 08:16:23 +0800

    update

---
+++ app/assets/js/all.js
@@ -0,0 +1,21 @@
+// 設置一個陣列，用來存放所有資料
+let data = [];
+
+// 獲取清單列表的 ul
+const list = document.querySelector('.list');
+
+// 預設為第一個 tab (分頁標籤)為全部
+let toggleTab = 'all';
+
+// 新增
+const btn = document.querySelector('.btn_add');
+const input = document.querySelector('#input');
+btn.addEventListener('click', function(e){
+  // 取消預設事件 不然每次刪除會跑到連結 # 然後網頁會飛到最上面去
+  e.preventDefault();
+  if(input.value.trim() == ''){
+    alert('此項目名稱不得為空');
+    return;
+  }
+});
+
