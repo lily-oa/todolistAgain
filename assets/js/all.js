c0e10e8d3f4e69ba4e922245929696696d17d3ec
lily <a0976198586@gmail.com>
2022-10-10 12:13:42 +0800

    Update 2022-10-10T04:13:40.720Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // 設置一個陣列，用來存放所有資料
-var data = []; // 獲取清單列表的 ul
+var data = []; // 獲取清單列表的 ul li
 
 var list = document.querySelector('.list'); // 預設為第一個 ul.tab>li.active   (分頁標籤)為全部
 
@@ -10,7 +10,7 @@ var toggleTab = 'all'; // 新增
 var btn = document.querySelector('.btn_add');
 var input = document.querySelector('#input');
 btn.addEventListener('click', function (e) {
-  // 取消預設事件 不然每次刪除會跑到連結 # 然後網頁會飛到最上面去
+  // 取消預設事件 因為是超連結(a)，每次刪除會自動跳轉網頁
   e.preventDefault();
 
   if (input.value.trim() == '') {
@@ -45,14 +45,13 @@ function renderData() {
       count += 1;
 
       if (toggleTab == 'all' || toggleTab == 'work') {
-        str += "\n                <li>\n                  <label for=\"\" class=\"checkbox\" for=''>\n                    <input type=\"checkbox\" data-num=\"".concat(index, "\">\n                    <span>").concat(item.content, "</span>\n                  </label>\n                  <a href=\"#\" class=\"delete\" data-num=\"").concat(index, "\"></a>\n                </li>\n        ");
+        str += "\n                <li>\n                  <label for=\"\" class=\"checkbox\">\n                    <input type=\"checkbox\" data-num=\"".concat(index, "\">\n                    <span>").concat(item.content, "</span>\n                  </label>\n                  <a href=\"#\" class=\"delete\" data-num=\"").concat(index, "\"></a>\n                </li>\n        ");
       }
     } else if (item.checked && toggleTab == 'all' || item.checked && toggleTab == 'done') {
-      str += "\n              <li>\n                <label for=\"\" class=\"checkbox\" for=''>\n                  <input type=\"checkbox\" data-num=\"".concat(index, "\">\n                  <span>").concat(item.content, "</span>\n                </label>\n                <a href=\"#\" class=\"delete\" data-num=\"").concat(index, "\"></a>\n              </li>\n      ");
+      str += "\n              <li>\n                <label for=\"\" class=\"checkbox\">\n                  <input type=\"checkbox\" checked data-num=\"".concat(index, "\">\n                  <span>").concat(item.content, "</span>\n                </label>\n                <a href=\"#\" class=\"delete\" data-num=\"").concat(index, "\"></a>\n              </li>\n      ");
     }
-  });
-  console.log(data);
-  list.innerHTML = str;
-  todoLength.textContent = count;
+  }); // console.log(data);
+
+  list.innerHTML = str; // todoLength.textContent = count;
 }
 //# sourceMappingURL=all.js.map
